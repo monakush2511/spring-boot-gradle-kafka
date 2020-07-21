031f7e73c7832db2e3d4cf4fc13f50ca81c77258
ADMIN <ADMIN@DESKTOP-12J3LDS>
2020-07-21 21:26:45 +0530

    CODE 3- SpringBoot Maven Kafka Consumer Code

---
+++ spring-boot-consumer-kafka/src/main/java/com/springboot/kafka/consumer/controllers/KafkaConsumerService.java
@@ -0,0 +1,27 @@
+package com.springboot.kafka.consumer.controllers;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.springboot.kafka.consumer.entities.User;
+
+@Service
+public class KafkaConsumerService {
+
+	
+	
+	  @KafkaListener(topics="kafkatopic3",
+	  containerFactory="kafkaListenerContainerFactory", groupId="string_group")
+	  public void stringConsume(String message) {
+	  System.out.println("String message consumed!!" +message); 
+	  }
+	  
+	  @KafkaListener(topics="kafkatopic4",containerFactory="jsonKafkaListenerContainerFactory", groupId="json_group")
+	  public void jsonConsume(User user) { 
+		  System.out.println("Json message consumed!!" +user);
+	  }
+	 
+	  
+	 
+	
+}
